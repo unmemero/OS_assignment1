ddd4f630a76beb545593113cb582e21f6cbb29e3
Rafael Garcia <fafagm99@gmail.com>
2024-09-23 17:25:30 -0600

    Completed and tested head and tail

--- head.c
+++ head.c
@@ -5,37 +5,90 @@
 #include <errno.h>
 #include <stdint.h>
 #include <string.h>
-#include "better_write.h"
 
 /*
-    Better write
-    Used for printing
-    Remember to include actual function
+    Better write function
+    To display all bytes requested in STDOUT
 */
+ssize_t better_write(int fd, const char *buf, size_t count) {
+  size_t already_written, to_be_written, written_this_time;
+  ssize_t res_write;
 
+  if (count == ((size_t) 0)) return (ssize_t) count;
+  
+  already_written = (size_t) 0;
+  to_be_written = count;
+  while (to_be_written > ((size_t) 0)) {
+    res_write = write(fd, &(((const char *) buf)[already_written]), to_be_written);
+    if (res_write < ((size_t) 0)) {
+      /* Error */
+      return res_write;
+    }
+    if (res_write == ((ssize_t) 0)) {
+      /* Nothing written, stop trying */
+      return (ssize_t) already_written;
+    }
+    written_this_time = (size_t) res_write;
+    already_written += written_this_time;
+    to_be_written -= written_this_time;
+  }
+  return (ssize_t) already_written;
+}
 
 /*
-    Custom string length function
-    Remember to include actual function
+    To check if a string is a number
 */
+int is_number(const char *arg) {
+    int i = 0;
+    if (arg[0] == '\0') return 0;
+    if (arg[0] == '-') {
+        i = 1;
+        if (arg[1] == '\0') return 0;
+    }
+    while (arg[i] != '\0') {
+        if (arg[i] < '0' || arg[i] > '9') return 0;
+        i++;
+    }
+    return 1;
+}
 
 /*
-    Check if -n is the argument
-    add actual function
+    To determine size of string
 */
-
+int my_strlen(const char *arg){
+    int i = 0;
+    while(arg[i] != '\0') i++;
+    return i;
+}
 
 /*
-    Check if argument is a number
-    Add actual function
+    To compare two strings
 */
+int my_strcmp(const char *arg, const char *cmp){
+    while(*arg != '\0' && *cmp != '\0'){
+        if(*arg != *cmp) return 1;
+        arg++;
+        cmp++;
+    }
+    if(*arg == '\0' && *cmp == '\0') return 0;
+    return 1;
+}
 
 /*
-    Convert string to int
-    Add actual function
+    To convert string to integer
 */
+int my_atoi(const char *arg){
+    int result = 0;
+    if (arg[0] == '-') result = -1;
+    for(int i = 0; arg[i] != '\0'; i++){
+        result = result * 10 + arg[i] - '0';
+    }
+    return result;
+}
 
-/*To process when head has no arguments*/
+/*
+    To process when head has no arguments
+*/
 int process_input(const char* filename, ssize_t max_num_lines){
     if(max_num_lines > (ssize_t)INT64_MAX){
         better_write(STDERR_FILENO,"Error: Memory requirement too big\n",sizeof("Error: Memory requirement too big\n"));
--- tail.c
+++ tail.c
@@ -4,23 +4,90 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
-#include "better_write.h"
 
-/*Add functions and comments from better_write.c*/
+/*
+    Better write function
+    To display all bytes requested in STDOUT
+*/
+ssize_t better_write(int fd, const char *buf, size_t count) {
+  size_t already_written, to_be_written, written_this_time;
+  ssize_t res_write;
 
-/*Garbage collector*/
-void cleanup_and_return(char *current_line, char **lines, size_t *line_lengths, int fd){
-    if(current_line != NULL) free(current_line);
-    if(lines != NULL){
-        for(size_t i = 0; i<sizeof(lines);i++) if(lines[i] != NULL) free(lines[i]);
-        free(lines);
-        free(line_lengths);
+  if (count == ((size_t) 0)) return (ssize_t) count;
+  
+  already_written = (size_t) 0;
+  to_be_written = count;
+  while (to_be_written > ((size_t) 0)) {
+    res_write = write(fd, &(((const char *) buf)[already_written]), to_be_written);
+    if (res_write < ((size_t) 0)) {
+      /* Error */
+      return res_write;
     }
-    if(fd != STDIN_FILENO) close(fd);
+    if (res_write == ((ssize_t) 0)) {
+      /* Nothing written, stop trying */
+      return (ssize_t) already_written;
+    }
+    written_this_time = (size_t) res_write;
+    already_written += written_this_time;
+    to_be_written -= written_this_time;
+  }
+  return (ssize_t) already_written;
 }
 
+/*
+    To check if a string is a number
+*/
+int is_number(const char *arg) {
+    int i = 0;
+    if (arg[0] == '\0') return 0;
+    if (arg[0] == '-') {
+        i = 1;
+        if (arg[1] == '\0') return 0;
+    }
+    while (arg[i] != '\0') {
+        if (arg[i] < '0' || arg[i] > '9') return 0;
+        i++;
+    }
+    return 1;
+}
+
+/*
+    To determine size of string
+*/
+int my_strlen(const char *arg){
+    int i = 0;
+    while(arg[i] != '\0') i++;
+    return i;
+}
+
+/*
+    To compare two strings
+*/
+int my_strcmp(const char *arg, const char *cmp){
+    while(*arg != '\0' && *cmp != '\0'){
+        if(*arg != *cmp) return 1;
+        arg++;
+        cmp++;
+    }
+    if(*arg == '\0' && *cmp == '\0') return 0;
+    return 1;
+}
+
+/*
+    To convert string to integer
+*/
+int my_atoi(const char *arg){
+    int result = 0;
+    if (arg[0] == '-') result = -1;
+    for(int i = 0; arg[i] != '\0'; i++){
+        result = result * 10 + arg[i] - '0';
+    }
+    return result;
+}
+
+
 /*Processing file*/
-int process_file(const char* filename, ssize_t max_num_lines){
+int process_input(const char* filename, ssize_t max_num_lines){
     /*Check where to read from*/
     int fd;
     if(filename == NULL){
@@ -28,146 +95,126 @@ int process_file(const char* filename, ssize_t max_num_lines){
     }else{
         fd = open(filename, O_RDONLY);
         if(fd<0){
-            fprintf(stderr,"Error opening file %s\n",strerror(errno));
+            better_write(STDERR_FILENO, "Error opening file \n", sizeof("Error opening file \n"));
             return 1;
         }
     }
 
-    /*Initialize arrays and pointers for current line and all lines*/
-    int return_code = 0;
-    char buffer[4096], *current_line = NULL, current_char, **lines = NULL;
-    void *pointer;
-    ssize_t read_result;
-    size_t chars_read, current_line_size = (size_t)0, current_line_allocation = (size_t)0, new_size;
-    size_t num_lines = (size_t)0, lines_allocated = (size_t)0, *line_lengths = NULL;
+    /*Initialize calllocated buffer*/
+    size_t buffer_size = 8192;
+    char *buffer = calloc(buffer_size, sizeof(char));
+    if(buffer == NULL){
+        better_write(STDERR_FILENO, "Error allocating buffer\n", sizeof("Error allocating buffer\n"));
+        if(fd != STDIN_FILENO) close(fd);
+        return 1;
+    }
 
-    /*Loop until the number of lines reaches the amount desired by the user*/
-    while(num_lines<max_num_lines){
+    /*Read until EOF*/
+    ssize_t total_read =(ssize_t)0;
+    ssize_t read_bytes;
+    while((read_bytes = read(fd, buffer + total_read, buffer_size - total_read)) > 0){
+        total_read += read_bytes;
 
-        /*
-            Read from the file and handle errors
-        */
-        if((read_result = read(fd, buffer, sizeof(buffer))<0)){
-            fprintf(stderr,"Error reading from file %s\n",strerror(errno));
-            cleanup_and_return(current_line, lines, line_lengths, fd);
-            return 1;
-        }
-
-        /*End of file*/
-        if(read_result == (size_t)0) break;
-
-        /*Loop through the buffer*/
-        for(size_t i = (size_t)0 ; i<(size_t)read_result; i++){
-            current_char = buffer[i];
-
-            /*Empty line case*/
-            if(current_line == NULL){
-                current_line_allocation = (size_t)1;
-
-                if((pointer = calloc(current_line_allocation, sizeof(char))) == NULL){
-                    fprintf(stderr,"Error allocating memory, %s\n", strerror(errno));
-                    cleanup_and_return(current_line, lines, line_lengths, fd);
-                    return 1;
-                }
-                current_line = (char*)pointer;
-            }else{
-                /*
-                    Subsequent characters
-                    First case is we need more memory
-                */
-                if(current_line_size >= current_line_allocation){
-                    new_size = current_line_allocation + current_line_allocation;
-                    if(new_size < current_line_allocation){
-                        fprintf(stderr,"Error: too much memory required, %s\n", strerror(errno));
-                        cleanup_and_return(current_line, lines, line_lengths, fd);
-                        return 1;
-                    }
-                    if((pointer = realloc(current_line, new_size*sizeof(char))) == NULL){
-                        fprintf(stderr,"Error reallocating memory, %s\n", strerror(errno));
-                        cleanup_and_return(current_line, lines, line_lengths, fd);
-                        return 1;
-                    }
-                    current_line = (char*)pointer;
-                    current_line_allocation = new_size;
-                }
+        /*Full buffer*/
+        if(total_read == buffer_size){
+            buffer_size += buffer_size;
+            char *new_buffer = realloc(buffer, buffer_size);
+            if(new_buffer == NULL){
+                better_write(STDERR_FILENO,"Error allocating memory\n",sizeof("Error allocating memory\n"));
+                free(buffer);
+                if(fd != STDIN_FILENO) close(fd);
+                return 1;
             }
+            buffer = new_buffer;
+        }
+    }
 
-            /*There's at least 1 byte left to include in*/
-            current_line[current_line_size] = current_char;
-            current_line_size++;
+    if(read_bytes < 0){
+        better_write(STDERR_FILENO,"Error reading file\n",sizeof("Error reading file\n"));
+        free(buffer);
+        if(fd != STDIN_FILENO) close(fd);
+        return 1;
+    }
 
-            /*Handle newline character*/
-            if(current_char == '\n'){
-                if(lines == NULL){
-                    lines_allocated = (size_t)1;
-                    if((pointer = calloc(lines_allocated, sizeof(char*))) == NULL){
-                        fprintf(stderr,"Error allocating memory, %s\n", strerror(errno));
-                        cleanup_and_return(current_line, lines, line_lengths, fd);
-                        return 1;
-                    }
-                    lines = (char**)pointer;
-                    if((pointer = calloc(lines_allocated, sizeof(size_t)))==NULL){
-                        fprintf(stderr,"Error allocating memory, %s\n", strerror(errno));
-                        cleanup_and_return(current_line, lines, line_lengths, fd);
-                        return 1;
-                    }
-                    line_lengths = (size_t*)pointer;
-                }else{
-                    if(num_lines >= lines_allocated){
-                        if((new_size = lines_allocated + lines_allocated)<lines_allocated){
-                            fprintf(stderr,"Error: too much memory required, %s\n", strerror(errno));
-                            cleanup_and_return(current_line, lines, line_lengths, fd);
-                            return 1;
-                        }
-                        if((pointer = realloc(lines, new_size*sizeof(char*))) == NULL){
-                            fprintf(stderr,"Error reallocating memory, %s\n", strerror(errno));
-                            cleanup_and_return(current_line, lines, line_lengths, fd);
-                            return 1;
-                        }
-                        lines = (char**)pointer;
-                        if((pointer = realloc(line_lengths, new_size*sizeof(size_t))) == NULL){
-                            fprintf(stderr,"Error reallocating memory, %s\n", strerror(errno));
-                            cleanup_and_return(current_line, lines, line_lengths, fd);
-                            return 1;
-                        }
-                        line_lengths = (size_t*)pointer;
-                        lines_allocated = new_size;
-                    }
-                }
-                lines[num_lines] = current_line;
-                line_lengths[num_lines] = current_line_size;
-                num_lines++;
-                current_line = NULL;
-                current_line_size = (size_t)0;
-                current_line_allocation = (size_t)0;
+    /*Process last lines of file*/
+    ssize_t num_lines = 0;
+    for(ssize_t i = total_read - 1;i>-1;i--){
+        if(buffer[i] == '\n'){
+            num_lines++;
+            if(num_lines == max_num_lines){
+                better_write(STDOUT_FILENO,buffer + i + 1, total_read - i - 1);
+                free(buffer);
+                if(fd != STDIN_FILENO) close(fd);
+                return 0;
             }
         }
     }
-    
-    /*Line with no \n, EOF case*/
-    if(current_line != NULL) {
-        if(lines == NULL){
-            lines_allocated = (size_t)1;
-            if((pointer = calloc(lines_allocated, sizeof(char*))) == NULL){
-                fprintf(stderr,"Error allocating memory, %s\n", strerror(errno));
-                cleanup_and_return(current_line, lines, line_lengths, fd);
-                return 1;
-            }
-            lines = (char**)pointer;
-            if((pointer = calloc(lines_allocated, sizeof(size_t))) == NULL){
-                fprintf(stderr,"Error allocating memory, %s\n", strerror(errno));
-                cleanup_and_return(current_line, lines, line_lengths, fd);
-                return 1;
-            }
-            line_lengths = (size_t*)pointer;
-        }else{
-            
-        }
-    }
+
+    better_write(STDOUT_FILENO,buffer,total_read);
+    free(buffer);
+    if(fd != STDIN_FILENO) close(fd);
     return 0;
 }
 
 int main(int argc, char const *argv[]){
-    
-    return 0;
+    ssize_t num_lines = 10;
+    const char *filename = NULL;
+
+    /*No arguments*/
+    if(argc == 1){
+        return process_input(filename, num_lines);
+    }
+
+    /*1 argument, treat as filename*/
+    if(argc == 2){
+        filename = argv[1];
+        return process_input(filename, num_lines);
+    }
+
+    /*3 args. Check if first one is -n*/
+    if(argc == 3){
+        if(!my_strcmp(argv[1],"-n")){
+            if(is_number(argv[2])){
+                num_lines = my_atoi(argv[2]);
+                return process_input(filename, num_lines);
+            }else{
+                better_write(STDERR_FILENO,"Error: not a number\n",sizeof("Error: not a number\n"));
+                return 1;
+            }
+        }else{
+            better_write(STDERR_FILENO,"Error: Illegal use of -n\n",sizeof(sizeof("Error: Illegal use of -n\n")));
+            return 1;
+        }
+    }
+
+    /*4 args or more args. Check if first one is -n*/
+    if(argc > 3){
+        if(!my_strcmp(argv[1],"-n")){
+            if(is_number(argv[2])){
+                num_lines = my_atoi(argv[2]);
+                filename = argv[3];
+                return process_input(filename, num_lines);
+            }else{
+                better_write(STDERR_FILENO,"Error: Number required for argument after -n\n",sizeof("Error: Number required for argument after -n\n"));
+                return 1;
+            }
+        }else{
+            filename = argv[1];
+            if(!my_strcmp(argv[2],"-n")){
+                if(is_number(argv[3])){
+                    num_lines = my_atoi(argv[3]);
+                    return process_input(filename, num_lines);
+                }else{
+                    better_write(STDERR_FILENO,"Error: Number required for argument after -n\n",sizeof("Error: Number required for argument after -n\n"));
+                    return 1;
+                }
+            }else{
+                better_write(STDERR_FILENO,"Error: Illegal use of -n\n",sizeof("Error: Illegal use of -n\n"));
+                return 1;
+            }
+        }
+    }
+
+    better_write(STDERR_FILENO,"Too many arguments\n",sizeof("Too many arguments\n"));
+    return 1;
 }
